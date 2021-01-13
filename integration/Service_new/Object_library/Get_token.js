429cd6cd90f1e9dea8e90504cb50ff6dfe143d6a
@valeraparafin <iparafin@gmail.com>
2021-01-13 10:18:11 +0300

    miss auth file

---
+++ integration/Service_new/Object_library/Get_token.js
@@ -0,0 +1,47 @@
+ export class getToken {
+    login = 'iparafin@yandex.ru' ;
+    password = 'A123321b';
+    token = null;
+    c = null;
+
+     Authorize () {
+        cy.visit('https://hq.asodesk.com');
+        cy.get('.mt5 > :nth-child(1) > .mt15')
+            .type(this.login).should('have.value', this.login)
+
+        cy.get(':nth-child(2) > .mt15')
+            .type(this.password).should('have.value', this.password)
+        cy.get('.mt5 > .accountButton').should('not.be.disabled').click()
+
+        cy.get('.app .ng-scope').should('be.visible')
+
+        //if the content on page is visible then get the cookies
+        //cause we need a delay to get complete data, before send request
+
+        cy.getCookie('sessionid')
+            .should('exist')
+            .then((cookie) => {
+                this.c = cookie.value
+                // cookie is an object with "domain", "name" and other properties+
+                // in this case only is an object with "sessionsid" value
+            })
+
+        cy.request({
+            method: 'POST',
+            url: 'https://hq.asodesk.com/api/auth/obtain', // baseUrl is prepended to url
+            form: true, // indicates the body should be form urlencoded and sets Content-Type: application/x-www-form-urlencoded headers
+            body: {
+                username: this.login,
+                password: this.password,
+            }
+        })
+            .then((response) => {
+                // console.log(response.body);
+                assert.equal(response.status, 200);
+                chai.expect(response.body.token).to.not.be.eq(null)
+                let jsonData = response.body;
+                this.token = jsonData.access;
+                // console.log(token);
+            })
+    };
+}
