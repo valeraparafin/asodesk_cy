d6f0cfc62674768a5529a2315af68190abb47e64
@valeraparafin <iparafin@gmail.com>
2021-02-20 15:11:37 +0300

    Refactoring code with zeeryne. Lets make code better :)

--- integration/Service_new/App_Store/CatRank-iphone.js
+++ integration/Service_new/App_Store/CatRank-iphone.js
@@ -9,46 +9,40 @@ const password = 'A123321b'
 let todaysDate = dayjs().valueOf() - 14400000;
 let prevdaysDate = dayjs().unix() - 200000;
 
-const favcountryID = ["RU", "US", "GB", "DE", "ES", "IT", "FR", "CA", "AU", "BR", "CN"]
+const favcountryIDs = ["RU", "US", "GB", "DE", "ES", "IT", "FR", "CA", "AU", "BR", "CN"]
 // const favcountryID = ["bb", "uz", "ch", "es", "lb", "ng", "mx", "eg", "cm", "uy", "th", "my", "bm", "cn", "gb", "il", "ec", "is", "hk", "in", "tw", "si", "nz", "ua", "cz", "do", "kw", "tr", "kr", "rs", "it", "vn", "sk", "sv", "no", "bh", "al", "ca", "kz", "qa", "ph", "pe", "by", "sa", "fr", "ge", "om", "kg", "ke", "hr", "kh", "ve", "ie", "pa", "az", "dz", "gt", "am", "py", "mt", "au", "be", "ae", "at", "hn", "za", "nl", "ru", "cy", "gh", "ar", "sg", "mo", "lk", "dk", "ni", "br", "bg", "pk", "lt", "sn", "cl", "us", "cr", "mg", "ao", "lu", "pt", "fi", "pl", "de", "jp", "co", "ee", "hu", "gr", "ug", "se", "tn", "lv", "ro", "id", "bo"]
 
 // тестовая дата в разрыв
 // let prevdaysDate = 1597766884;
 // let todaysDate = 1598086871;
 
-let country = new Map([
-    ['Russia', "ru"],
-    ['USA', "us"]
-])
 let deviceType = new Map([
     ['iphone', "iphone"],
     ['ipad', "ipad"],
     ['android', "googleplay"]
 ])
-let appCategoryID = new Map([
-    ['aps', "6011"],
-    ['aps-cn', "6007"],
-    ['gp', "MUSIC_AND_AUDIO"]
+let appCategoryIDs = new Map([
+    ['default', "6011"],
+    ['CN', "6007"]
 ])
 
-let storeID = new Map([
-    ['aps', "324684580"],
-    ['aps-cn', "388627783"],
-    ['gp', "com.spotify.music"]
+let storeIDs = new Map([
+    ['default', "324684580"],
+    ['CN', "388627783"]
 ])
 
 describe('AppStore CatRank Health-check', () => {
-    for (let i = 0; i <= favcountryID.length - 1; i++) {
 
-        context('Check response from Category Ranking iPhone. Local ' + favcountryID[i], () => {
+    for (let country of favcountryIDs) {
+        context('Check response from Category Ranking iPhone. Local ' + country, () => {
             beforeEach(() => {
                 cy.visit('https://hq.asodesk.com')
             })
             it('Category Ranking API. Check data on y != null', () => {
-                https://on.cypress.io/type
+                // https://on.cypress.io/type
 
-                    cy.get('.mt5 > :nth-child(1) > .mt15')
-                        .type(login).should('have.value', login)
+                cy.get('.mt5 > :nth-child(1) > .mt15')
+                    .type(login).should('have.value', login)
 
                 cy.get(':nth-child(2) > .mt15')
                     .type(password).should('have.value', password)
@@ -56,244 +50,40 @@ describe('AppStore CatRank Health-check', () => {
                 cy.get('.mt5 > .accountButton').should('not.be.disabled').click();
                 cy.get('.app .ng-scope').should('be.visible')
 
+                let storeID = storeIDs.get(country) || storeIDs.get('default');
+                console.log(storeID);
 
-                if (favcountryID[i] === 'CN') {
-                    cy.request({
-                        method: 'get',
-                        followRedirect: false, log: true, //turn off
-                        url: 'api/category-ranking/chart?category=' + appCategoryID.get('aps-cn') + '&category_list=free&country=' + favcountryID[i] + '&device_type=' + deviceType.get('iphone') + '&storeids=' + storeID.get('aps-cn') + '&timestamp_since=' + prevdaysDate,
-                        headers: {
-                            'accept': 'application/json'
-                        },
-                        response: []
+                let appCategoryID = appCategoryIDs.get(country) || appCategoryIDs.get('default');
+                console.log(appCategoryID);
+
+                cy.request({
+                    method: 'get',
+                    followRedirect: false, log: true, //turn off
+                    url: 'api/category-ranking/chart?category=' + appCategoryID + '&category_list=free&country=' + country + '&device_type=' + deviceType.get('iphone') + '&storeids=' + storeID + '&timestamp_since=' + prevdaysDate,
+                    headers: {
+                        'accept': 'application/json'
+                    },
+                    response: []
+                })
+                    .then((response) => {
+
+                        console.log(response.body);
+                        assert.equal(response.status, 200);
+
+                        if ((response.body.data.length) === 0) {
+                            chai.expect(response.body.data.length).to.not.be.eq(0)
+                        }
+                        // if response json is empty
+                        else {
+                            chai.expect(response.body.data[0].stats[0].y).to.not.be.eq(null)
+                        }
+                        // else response json has data and this data is not equal null
+                        // mean has data and graph is alive
                     })
-                        .then((response) => {
 
-                            console.log(response.body);
-                            assert.equal(response.status, 200);
-
-                            if ((response.body.data.length) === 0) {
-                                chai.expect(response.body.data.length).to.not.be.eq(0)
-                            }
-                            // if response json is empty
-                            else {
-                                chai.expect(response.body.data[0].stats[0].y).to.not.be.eq(null)
-                            }
-                            // else response json has data and this data is not equal null
-                            // mean has data and graph is alive
-                        })
-
-                } else {
-                    cy.request({
-                        method: 'get',
-                        followRedirect: false, log: true, //turn off
-                        url: 'api/category-ranking/chart?category=' + appCategoryID.get('aps') + '&category_list=free&country=' + favcountryID[i] + '&device_type=' + deviceType.get('iphone') + '&storeids=' + storeID.get('aps') + '&timestamp_since=' + prevdaysDate,
-                        headers: {
-                            'accept': 'application/json'
-                        },
-                        response: []
-                    })
-                        .then((response) => {
-
-                            console.log(response.body);
-                            assert.equal(response.status, 200);
-
-                            if ((response.body.data.length) === 0) {
-                                chai.expect(response.body.data.length).to.not.be.eq(0)
-                            }
-                            // if response json is empty
-                            else {
-                                chai.expect(response.body.data[0].stats[0].y).to.not.be.eq(null)
-                            }
-                            // else response json has data and this data is not equal null
-                            // mean has data and graph is alive
-                        })
-                }
 
             })
         })
 
     }
 })
-
-
-/*
-context('Check response from Category Ranking iPhone. Local "RU" ', () => {
-    beforeEach(() => {
-        cy.visit('https://hq.asodesk.com')
-    })
-    it('Category Ranking API. Check data on y != null', () => {
-        // https://on.cypress.io/type
-
-        cy.get('.mt5 > :nth-child(1) > .mt15')
-            .type(login).should('have.value', login)
-
-        cy.get(':nth-child(2) > .mt15')
-            .type(password).should('have.value', password)
-
-        cy.get('.mt5 > .accountButton').click()
-        cy.wait(3000)
-        cy.request({
-            method: 'get',
-            followRedirect: false, log: true, //turn off
-
-            url: 'api/category-ranking/chart?category=' + appCategoryID.get('aps') + '&category_list=free&country=' + country.get('Russia') + '&device_type=' + deviceType.get('iphone') + '&storeids=' + storeID.get('aps') + '&timestamp_since=' + todaysDate,
-            headers: {
-                'accept': 'application/json'
-            },
-            response: []
-        })
-            .then((response) => {
-
-                console.log(response.body);
-                assert.equal(response.status, 200);
-
-                if ((response.body.data.length) === 0) {
-                    chai.expect(response.body.data.length).to.not.be.eq(0)
-                }
-                // if response json is empty
-                else {
-                    chai.expect(response.body.data[0].stats[0].y).to.not.be.eq(null)
-                }
-                // else response json has data and this data is not equal null
-                // mean has data and graph is alive
-            })
-    })
-})
-context('Check response from Category Ranking iPhone. Local "US" ', () => {
-    beforeEach(() => {
-        cy.visit('https://hq.asodesk.com')
-    })
-    it('Category Ranking API. Check data on y != null', () => {
-        // https://on.cypress.io/type
-
-        cy.get('.mt5 > :nth-child(1) > .mt15')
-            .type(login).should('have.value', login)
-
-        cy.get(':nth-child(2) > .mt15')
-            .type(password).should('have.value', password)
-
-        cy.get('.mt5 > .accountButton').click()
-        cy.wait(3000)
-        cy.request({
-            method: 'get',
-            followRedirect: true, log: true, //turn off
-
-            url: 'api/category-ranking/chart?category=' + appCategoryID.get('aps') + '&category_list=free&country=' + country.get('USA') + '&device_type=' + deviceType.get('iphone') + '&storeids=' + storeID.get('aps') + '&timestamp_since=' + todaysDate,
-            headers: {
-                'accept': 'application/json'
-            },
-            response: []
-        })
-            .then((response) => {
-
-                console.log(response.body);
-                assert.equal(response.status, 200);
-
-                if ((response.body.data.length) === 0) {
-                    chai.expect(response.body.data.length).to.not.be.eq(0)
-                }
-                // if response json is empty
-                else {
-                    chai.expect(response.body.data[0].stats[0].y).to.not.be.eq(null)
-                }
-                // else response json has data and this data is not equal null
-                // mean has data and graph is alive
-            })
-    })
-})
-
-context('Check response from Category Ranking Android. Local "RU" ', () => {
-    beforeEach(() => {
-        cy.visit('https://hq.asodesk.com')
-    })
-
-
-    it('Category Ranking API. Check data on y != null', () => {
-        // https://on.cypress.io/type
-
-
-        cy.get('.mt5 > :nth-child(1) > .mt15')
-            .type(login).should('have.value', login)
-
-        cy.get(':nth-child(2) > .mt15')
-            .type(password).should('have.value', password)
-
-        cy.get('.mt5 > .accountButton').click()
-        cy.wait(3000)
-
-        cy.request({
-            method: 'get',
-            followRedirect: true, log: true, //turn off
-
-            url: 'api/category-ranking/chart?category=' + appCategoryID.get('gp') + '&category_list=free&country=' + country.get('Russia') + '&device_type=' + deviceType.get('android') + '&storeids=' + storeID.get('gp') + '&timestamp_since=' + todaysDate,
-            headers: {
-                'accept': 'application/json'
-            },
-            response: []
-        })
-            .then((response) => {
-
-                console.log(response.body);
-                assert.equal(response.status, 200);
-
-                if ((response.body.data.length) === 0) {
-                    chai.expect(response.body.data.length).to.not.be.eq(0)
-                }
-                // if response json is empty
-                else {
-                    chai.expect(response.body.data[0].stats[0].y).to.not.be.eq(null)
-                }
-                // else response json has data and this data is not equal null
-                // mean has data and graph is alive
-            })
-    })
-})
-context('Check response from Category Ranking Android. Local "US" ', () => {
-    beforeEach(() => {
-        cy.visit('https://hq.asodesk.com')
-    })
-
-
-    it('Category Ranking API. Check data on y != null', () => {
-        // https://on.cypress.io/type
-
-
-        cy.get('.mt5 > :nth-child(1) > .mt15')
-            .type(login).should('have.value', login)
-
-        cy.get(':nth-child(2) > .mt15')
-            .type(password).should('have.value', password)
-
-        cy.get('.mt5 > .accountButton').click()
-        cy.wait(3000)
-
-        cy.request({
-            method: 'get',
-            followRedirect: true, log: true, //turn off
-
-            url: 'api/category-ranking/chart?category=' + appCategoryID.get('gp') + '&category_list=free&country=' + country.get('USA') + '&device_type=' + deviceType.get('android') + '&storeids=' + storeID.get('gp') + '&timestamp_since=' + todaysDate,
-            headers: {
-                'accept': 'application/json'
-            },
-            response: []
-        })
-            .then((response) => {
-
-                console.log(response.body);
-                assert.equal(response.status, 200);
-
-                if ((response.body.data.length) === 0) {
-                    chai.expect(response.body.data.length).to.not.be.eq(0)
-                    // if response json is empty
-                } else {
-                    chai.expect(response.body.data[0].stats[0].y).to.not.be.eq(null)
-                    // else response json has data and this data is not equal null
-                    // mean has data and graph is alive
-                }
-
-            })
-    })
-})  */
-
-
--- integration/Service_new/Google_Play/CatRank-android.js
+++ integration/Service_new/Google_Play/CatRank-android.js
@@ -1,48 +1,38 @@
-/// <reference types="cypress" />
+import 'dayjs';
 
-let prevdaysDate = Cypress.moment().format('X') - 200000
-let todaysDate = Cypress.moment().format('X') - 4800
+const dayjs = require('dayjs')
 
 const login = 'iparafin@yandex.ru'
 const password = 'A123321b'
 
+let todaysDate = dayjs().valueOf() - 14400000;
+let prevdaysDate = dayjs().unix() - 200000;
+
 const favcountryID = ["RU", "US", "GB", "DE", "ES", "IT", "FR", "CA", "AU", "BR", "CN"]
 // const favcountryID = ["bb", "uz", "ch", "es", "lb", "ng", "mx", "eg", "cm", "uy", "th", "my", "bm", "cn", "gb", "il", "ec", "is", "hk", "in", "tw", "si", "nz", "ua", "cz", "do", "kw", "tr", "kr", "rs", "it", "vn", "sk", "sv", "no", "bh", "al", "ca", "kz", "qa", "ph", "pe", "by", "sa", "fr", "ge", "om", "kg", "ke", "hr", "kh", "ve", "ie", "pa", "az", "dz", "gt", "am", "py", "mt", "au", "be", "ae", "at", "hn", "za", "nl", "ru", "cy", "gh", "ar", "sg", "mo", "lk", "dk", "ni", "br", "bg", "pk", "lt", "sn", "cl", "us", "cr", "mg", "ao", "lu", "pt", "fi", "pl", "de", "jp", "co", "ee", "hu", "gr", "ug", "se", "tn", "lv", "ro", "id", "bo"]
 
-// тестовая дата в разрыв
-// let prevdaysDate = 1597766884;
-// let todaysDate = 1598086871;
-
-let country = new Map([
-    ['Russia', "ru"],
-    ['USA', "us"]
-])
 let deviceType = new Map([
-    ['iphone', "iphone"],
-    ['ipad', "ipad"],
     ['android', "googleplay"]
 ])
-let appCategoryID = new Map([
-    ['aps', "6011"],
-    ['gp', "MUSIC_AND_AUDIO"]
+let appCategoryIDs = new Map([
+    ['default', "MUSIC_AND_AUDIO"]
 ])
 
-let storeID = new Map([
-    ['aps', "324684580"],
-    ['gp', "com.spotify.music"]
+let storeIDs = new Map([
+    ['default', "com.spotify.music"]
 ])
+
 describe('Android CatRank Health-check', () => {
-    for (let i = 0; i <= favcountryID.length - 1; i++) {
 
-        context('Check response from Category Ranking Android. Local ' + favcountryID[i], () => {
+    for (let country of favcountryID) {
+
+        context('Check response from Category Ranking Android. Local ' + country, () => {
             beforeEach(() => {
                 cy.visit('https://hq.asodesk.com')
             })
 
-
             it('Category Ranking API. Check data on y != null', () => {
                 // https://on.cypress.io/type
-
 
                 cy.get('.mt5 > :nth-child(1) > .mt15')
                     .type(login).should('have.value', login)
@@ -53,11 +43,14 @@ describe('Android CatRank Health-check', () => {
                 cy.get('.mt5 > .accountButton').should('not.be.disabled').click();
                 cy.get('.app .ng-scope').should('be.visible')
 
+                let storeID = storeIDs.get(country) || storeIDs.get('default');
+                let appCategoryID = appCategoryIDs.get(country) || appCategoryIDs.get('default');
+
                 cy.request({
                     method: 'get',
                     followRedirect: true, log: true, //turn off
 
-                    url: 'api/category-ranking/chart?category=' + appCategoryID.get('gp') + '&category_list=free&country=' + favcountryID[i] + '&device_type=' + deviceType.get('android') + '&storeids=' + storeID.get('gp') + '&timestamp_since=' + prevdaysDate,
+                    url: 'api/category-ranking/chart?category=' + appCategoryID + '&category_list=free&country=' + country + '&device_type=' + deviceType.get('android') + '&storeids=' + storeID + '&timestamp_since=' + prevdaysDate,
                     headers: {
                         'accept': 'application/json'
                     },
@@ -83,185 +76,3 @@ describe('Android CatRank Health-check', () => {
 
     }
 })
-
-
-/*
-context('Check response from Category Ranking iPhone. Local "RU" ', () => {
-    beforeEach(() => {
-        cy.visit('https://hq.asodesk.com')
-    })
-    it('Category Ranking API. Check data on y != null', () => {
-        // https://on.cypress.io/type
-
-        cy.get('.mt5 > :nth-child(1) > .mt15')
-            .type(login).should('have.value', login)
-
-        cy.get(':nth-child(2) > .mt15')
-            .type(password).should('have.value', password)
-
-        cy.get('.mt5 > .accountButton').click()
-        cy.wait(3000)
-        cy.request({
-            method: 'get',
-            followRedirect: false, log: true, //turn off
-
-            url: 'api/category-ranking/chart?category=' + appCategoryID.get('aps') + '&category_list=free&country=' + country.get('Russia') + '&device_type=' + deviceType.get('iphone') + '&storeids=' + storeID.get('aps') + '&timestamp_since=' + todaysDate,
-            headers: {
-                'accept': 'application/json'
-            },
-            response: []
-        })
-            .then((response) => {
-
-                console.log(response.body);
-                assert.equal(response.status, 200);
-
-                if ((response.body.data.length) === 0) {
-                    chai.expect(response.body.data.length).to.not.be.eq(0)
-                }
-                // if response json is empty
-                else {
-                    chai.expect(response.body.data[0].stats[0].y).to.not.be.eq(null)
-                }
-                // else response json has data and this data is not equal null
-                // mean has data and graph is alive
-            })
-    })
-})
-context('Check response from Category Ranking iPhone. Local "US" ', () => {
-    beforeEach(() => {
-        cy.visit('https://hq.asodesk.com')
-    })
-    it('Category Ranking API. Check data on y != null', () => {
-        // https://on.cypress.io/type
-
-        cy.get('.mt5 > :nth-child(1) > .mt15')
-            .type(login).should('have.value', login)
-
-        cy.get(':nth-child(2) > .mt15')
-            .type(password).should('have.value', password)
-
-        cy.get('.mt5 > .accountButton').click()
-        cy.wait(3000)
-        cy.request({
-            method: 'get',
-            followRedirect: true, log: true, //turn off
-
-            url: 'api/category-ranking/chart?category=' + appCategoryID.get('aps') + '&category_list=free&country=' + country.get('USA') + '&device_type=' + deviceType.get('iphone') + '&storeids=' + storeID.get('aps') + '&timestamp_since=' + todaysDate,
-            headers: {
-                'accept': 'application/json'
-            },
-            response: []
-        })
-            .then((response) => {
-
-                console.log(response.body);
-                assert.equal(response.status, 200);
-
-                if ((response.body.data.length) === 0) {
-                    chai.expect(response.body.data.length).to.not.be.eq(0)
-                }
-                // if response json is empty
-                else {
-                    chai.expect(response.body.data[0].stats[0].y).to.not.be.eq(null)
-                }
-                // else response json has data and this data is not equal null
-                // mean has data and graph is alive
-            })
-    })
-})
-
-context('Check response from Category Ranking Android. Local "RU" ', () => {
-    beforeEach(() => {
-        cy.visit('https://hq.asodesk.com')
-    })
-
-
-    it('Category Ranking API. Check data on y != null', () => {
-        // https://on.cypress.io/type
-
-
-        cy.get('.mt5 > :nth-child(1) > .mt15')
-            .type(login).should('have.value', login)
-
-        cy.get(':nth-child(2) > .mt15')
-            .type(password).should('have.value', password)
-
-        cy.get('.mt5 > .accountButton').click()
-        cy.wait(3000)
-
-        cy.request({
-            method: 'get',
-            followRedirect: true, log: true, //turn off
-
-            url: 'api/category-ranking/chart?category=' + appCategoryID.get('gp') + '&category_list=free&country=' + country.get('Russia') + '&device_type=' + deviceType.get('android') + '&storeids=' + storeID.get('gp') + '&timestamp_since=' + todaysDate,
-            headers: {
-                'accept': 'application/json'
-            },
-            response: []
-        })
-            .then((response) => {
-
-                console.log(response.body);
-                assert.equal(response.status, 200);
-
-                if ((response.body.data.length) === 0) {
-                    chai.expect(response.body.data.length).to.not.be.eq(0)
-                }
-                // if response json is empty
-                else {
-                    chai.expect(response.body.data[0].stats[0].y).to.not.be.eq(null)
-                }
-                // else response json has data and this data is not equal null
-                // mean has data and graph is alive
-            })
-    })
-})
-context('Check response from Category Ranking Android. Local "US" ', () => {
-    beforeEach(() => {
-        cy.visit('https://hq.asodesk.com')
-    })
-
-
-    it('Category Ranking API. Check data on y != null', () => {
-        // https://on.cypress.io/type
-
-
-        cy.get('.mt5 > :nth-child(1) > .mt15')
-            .type(login).should('have.value', login)
-
-        cy.get(':nth-child(2) > .mt15')
-            .type(password).should('have.value', password)
-
-        cy.get('.mt5 > .accountButton').click()
-        cy.wait(3000)
-
-        cy.request({
-            method: 'get',
-            followRedirect: true, log: true, //turn off
-
-            url: 'api/category-ranking/chart?category=' + appCategoryID.get('gp') + '&category_list=free&country=' + country.get('USA') + '&device_type=' + deviceType.get('android') + '&storeids=' + storeID.get('gp') + '&timestamp_since=' + todaysDate,
-            headers: {
-                'accept': 'application/json'
-            },
-            response: []
-        })
-            .then((response) => {
-
-                console.log(response.body);
-                assert.equal(response.status, 200);
-
-                if ((response.body.data.length) === 0) {
-                    chai.expect(response.body.data.length).to.not.be.eq(0)
-                    // if response json is empty
-                } else {
-                    chai.expect(response.body.data[0].stats[0].y).to.not.be.eq(null)
-                    // else response json has data and this data is not equal null
-                    // mean has data and graph is alive
-                }
-
-            })
-    })
-})  */
-
-
