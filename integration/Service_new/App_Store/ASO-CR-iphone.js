f8b0da90f7081d281a39813915becbe3788b6f82
@valeraparafin <iparafin@gmail.com>
2021-03-12 11:02:18 +0300

    Update ASO CR healthy - Check only last date.

--- integration/Service_new/App_Store/ASO-CR-iphone.js
+++ integration/Service_new/App_Store/ASO-CR-iphone.js
@@ -10,8 +10,9 @@ const GetToken = new getToken();
 const favCountryIds = ["RU", "US", "GB", "DE", "ES", "IT", "FR", "CA", "AU", "BR"];
 const favStoreIds = ["324684580", "585027354", "835599320", "447188370"]; //spotify, googlemaps, tiktok, snapchat
 
-let prevDaysDate = dayjs().unix() - 200000;
-let monthAgo = dayjs().unix() - 2739405;
+let prevDaysDate = dayjs().unix() - 10000; //  - 200000;
+let toDaysDate = dayjs().unix() - 4780; //
+//let monthAgo = dayjs().unix() - 2739405;
 
 let isNull;
 
@@ -30,7 +31,7 @@ describe('Healthy check ASO Comparative Report Chart', () => {
                         method: 'get',
                         followRedirect: false, log: true, //turn off
 
-                        url: 'api/' + country + '/' + storeId + '/comparative-report-chart?time_since=' + monthAgo + '&time_till=' + prevDaysDate,
+                        url: 'api/' + country + '/' + storeId + '/comparative-report-chart?time_since=' + prevDaysDate + '&time_till=' + toDaysDate,
                         headers: {
                             'Authorization': 'Token:' + GetToken.token,
                             'sessionid': '' + GetToken.c //sessionid from cookies
@@ -42,6 +43,7 @@ describe('Healthy check ASO Comparative Report Chart', () => {
                                 let monthData = response.body.data[j];
 
                                 context('Find me a worsest day', () => {
+                                    //Check only last day
                                     if ((monthData.data[1] + monthData.data["2-5"] + monthData.data["6-10"] + monthData.data["11-20"] + monthData.data["21-50"] + monthData.data["51-100"]) === 0) {
                                         isNull = true
                                         chai.expect(isNull).to.be.false
