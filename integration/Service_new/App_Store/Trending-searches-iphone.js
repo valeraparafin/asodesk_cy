71476c9cc3f91af5fe967c62d3a80645831b66eb
@valeraparafin <iparafin@gmail.com>
2021-08-05 14:52:24 +0300

    New trending search healthy tests

---
+++ integration/Service_new/App_Store/Trending-searches-iphone.js
@@ -0,0 +1,74 @@
+import {Auth} from "../Classes_library/Auth";
+import {Constants} from "../Classes_library/Constants";
+import 'dayjs';
+
+const dayjs = require('dayjs');
+const auth = new Auth();
+const constant = new Constants();
+
+const favCountryIds = constant.AsFavCountryIds;
+const prevDaysDate = constant.s_toDaysDate;
+const deviceType = constant.AsDeviceType;
+
+let resp = null;
+
+describe('Trending Searches Health-check.', () => {
+    it('Obtain token', function () {
+        auth.obtain();
+    });
+
+    for (let country of favCountryIds) {
+        context('Trending Searches. Device: ' + deviceType + '. Local: ' + country, () => {
+            it('Response should equal 200', () => {
+                cy.request({
+                    method: 'get',
+                    followRedirect: false, log: true, //turn off
+                    url: 'api/' + country + '/trending-searches/?device=' + deviceType + '&timestamp=' + prevDaysDate,
+                    headers: {
+                        'accept': 'application/json',
+                        'Authorization': 'Token:' + auth.token,
+                    },
+                    response: []
+                })
+                    .then((response) => {
+                        assert.equal(response.status, 200);
+                        resp = response.body;
+                        console.log(resp)
+                    })
+            })
+
+            context('Minimum 4 keywords in object', () => {
+                for (let f = 0; f <= 7; f++) {
+                    it('Ojbect #' + f + ' has min number of keywords = 4', () => {
+                        expect(resp.results[f].keywords.length).to.be.greaterThan(3);
+                    })
+                }
+
+            })
+
+            context('Minimum 3 hours gap between objects', () => {
+                for (let f = 0; f < 7; f++) {
+                    it('Ojbect #' + f + ' and Object #' + (f + 1) + ' has minimum hours gap = 3', () => {
+                        let first = dayjs(resp.results[f].timestamp).unix();
+                        let second = dayjs(resp.results[f + 1].timestamp).unix();
+                        let compare = dayjs(first - second).hour();
+                        expect(compare).to.be.greaterThan(2)
+                    })
+                }
+            })
+
+            context('Apps per keyword is equal 3', () => {
+                for (let f = 0; f <= 7; f++) {
+                    it('Ojbect #' + f + ' has 4 keywords with 3 apps', () => {
+                        for (let i = 0; i <= 3; i++) {
+                            let apps = resp.results[f].keywords[i].apps.length;
+                            console.log(apps + ' | ' + i);
+                            expect(apps).equal(3);
+                        }
+                    })
+                }
+            })
+
+        })
+    }
+})
