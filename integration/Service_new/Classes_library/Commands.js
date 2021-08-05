49a63582fc7c484891c1de54d04afec5af26025e
@valeraparafin <iparafin@gmail.com>
2021-08-05 17:43:14 +0300

    correct ==

--- integration/Service_new/Classes_library/Commands.js
+++ integration/Service_new/Classes_library/Commands.js
@@ -9,7 +9,7 @@ export class Commands {
         cy.get('.profileDropdown__toggle').click();
         cy.get('.dropdown-menu > [href="/settings/profile"]').click({waitForAnimations: false});
         cy.wait(500);
-        status != true ? cy.get('#id_is_clear_rank').uncheck({force: true}).should('not.be.checked') : cy.get('#id_is_clear_rank').check({force: true}).should('be.checked');
+        status !== true ? cy.get('#id_is_clear_rank').uncheck({force: true}).should('not.be.checked') : cy.get('#id_is_clear_rank').check({force: true}).should('be.checked');
         cy.wait(500);
         cy.get(':nth-child(7) > .col-xs-12 > .btn-success').click();
     }
