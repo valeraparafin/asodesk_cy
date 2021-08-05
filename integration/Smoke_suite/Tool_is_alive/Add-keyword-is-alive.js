de5169ae8db5469d914876b6b10d7370dfdabc21
@valeraparafin <iparafin@gmail.com>
2021-08-05 16:02:06 +0300

    New tests: track app & add keyword

---
+++ integration/Smoke_suite/Tool_is_alive/Add-keyword-is-alive.js
@@ -0,0 +1,90 @@
+/// <reference types="cypress" />
+import {Auth} from "../../Service_new/Classes_library/Auth";
+
+const auth = new Auth();
+let AppUrl = null;
+let AppId = null;
+let keywordId = null;
+
+describe('Search & Track/UnTrack Application should be alive and requests should response 200', function () {
+    it('Obtain token', function () {
+        auth.obtain();
+    })
+    it('Tracked App should response 200 and not be empty ', function () {
+        cy.request({
+            method: 'GET',
+            followRedirect: true, log: true, //turn off
+            url: 'api/tracked-apps?country=ru',
+            headers: {
+                'accept': 'application/json',
+                'Authorization': 'Token:' + auth.token,
+            },
+            response: []
+        })
+            .then((response) => {
+                expect(response.status).eq(200);
+                expect(response.body).not.be.empty;
+                AppId = response.body[0].store_id;
+            })
+    });
+    it('Add keywords should response 200 and contain success: added', function () {
+        cy.request({
+            method: 'POST',
+            followRedirect: true, log: true, //turn off
+            url: '/api/ru/' + AppId + '/add-keywords',
+            headers: {
+                'accept': 'application/json',
+                'Authorization': 'Token:' + auth.token,
+            },
+            body: {
+                "keywords": ["инвестиции в плюс"],
+                "color_id": 0,
+                "tab_id": null
+            },
+            response: []
+        })
+            .then((response) => {
+                expect(response.status).eq(200);
+                expect(response.body.success).to.be.contain('added');
+
+
+            })
+    });
+    it('Find keyword_ids in data-stats should response 200 and not be empty', function () {
+        cy.request({
+            method: 'GET',
+            followRedirect: true, log: true, //turn off
+            url: '/api/ru/' + AppId + '/keyword-analytics/data-stats',
+            headers: {
+                'accept': 'application/json',
+                'Authorization': 'Token:' + auth.token,
+            },
+            response: []
+        })
+            .then((response) => {
+                expect(response.status).eq(200);
+                let keywordData = response.body.data.find(item => item.keyword === "инвестиции в плюс")
+                expect(keywordData).not.be.empty;
+                keywordId = keywordData.id
+            })
+    });
+    it('Remove keywords should response 200 and contain success: removed ', function () {
+        cy.request({
+            method: 'POST',
+            followRedirect: true, log: true, //turn off
+            url: '/api/ru/' + AppId + '/remove-keywords',
+            headers: {
+                'accept': 'application/json',
+                'Authorization': 'Token:' + auth.token,
+            },
+            body: {
+                "keyword_ids": [keywordId],
+            },
+            response: []
+        })
+            .then((response) => {
+                expect(response.status).eq(200);
+                expect(response.body.success).to.be.contain('removed');
+            })
+    });
+});
---
+++ integration/Smoke_suite/Tool_is_alive/Track-App-is-alive.js
@@ -0,0 +1,64 @@
+/// <reference types="cypress" />
+import {Auth} from "../../Service_new/Classes_library/Auth";
+
+const auth = new Auth();
+let AppUrl = null;
+let AppId = null;
+describe('Search & Track/UnTrack Application should be alive and requests should response 200', function () {
+    it('Obtain token', function () {
+        auth.obtain();
+    })
+    it('Search App should response 200 and not be empty ', function () {
+        cy.request({
+            method: 'GET',
+            followRedirect: true, log: true, //turn off
+            url: 'api/ru/search-application?request_id=1627465316072&query=трек+номер',
+            headers: {
+                'accept': 'application/json',
+                'Authorization': 'Token:' + auth.token,
+            },
+            response: []
+        })
+            .then((response) => {
+                expect(response.status).eq(200);
+                expect(response.body.data).not.be.empty;
+                AppUrl = response.body.data[0].url;
+                AppId = response.body.data[0].store_id;
+            })
+    });
+    it('Track App should response 200 and has success: tracked', function () {
+        cy.request({
+            method: 'POST',
+            followRedirect: true, log: true, //turn off
+            url: 'api/ru/track-app-by-url',
+            headers: {
+                'accept': 'application/json',
+                'Authorization': 'Token:' + auth.token,
+            },
+            body: {
+                "app_url": AppUrl,
+            },
+            response: []
+        })
+            .then((response) => {
+                expect(response.status).eq(200);
+                expect(response.body.success).to.contain('tracked');
+            })
+    });
+    it('UnTrack App should response 200 and has success: removed', function () {
+        cy.request({
+            method: 'POST',
+            followRedirect: true, log: true, //turn off
+            url: 'api/ru/' + AppId + '/untrack-app',
+            headers: {
+                'accept': 'application/json',
+                'Authorization': 'Token:' + auth.token,
+            },
+            response: []
+        })
+            .then((response) => {
+                expect(response.status).eq(200);
+                expect(response.body.success).to.contain('removed');
+            })
+    });
+});
