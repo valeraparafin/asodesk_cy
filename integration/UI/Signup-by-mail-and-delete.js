54438db8dba718f559137f18c91c0bc51667df14
@valeraparafin <iparafin@gmail.com>
2021-11-03 00:03:32 +0300

    Create and delete slurp inbox. Increase timeout.

--- integration/UI/Signup-by-mail-and-delete.js
+++ integration/UI/Signup-by-mail-and-delete.js
@@ -1,8 +1,8 @@
 /// <reference types="cypress-mailslurp" />
 
 import "cypress-mailslurp";
-import {Commands} from "../Service_new/Classes_library/Commands";
-import {Auth} from "../Service_new/Classes_library/Auth";
+import { Commands } from "../Service_new/Classes_library/Commands";
+import { Auth } from "../Service_new/Classes_library/Auth";
 
 const command = new Commands();
 const auth = new Auth();
@@ -11,7 +11,8 @@ describe("User case sign up, change password and delete account", function () {
     // use cypress-mailslurp plugin to create or use created an email address before test
     before(function () {
         return cy.mailslurp()
-            .then(mailslurp => mailslurp.getInbox('9c241587-6cac-4d5e-a3c4-c1dd264890bd'))
+            // create new inbox
+            .then(mailslurp => mailslurp.createInbox())
             .then(inbox => {
                 // save inbox id and email address to this (make sure you use function and not arrow syntax)
                 cy.wrap(inbox.id).as('inboxId')
@@ -31,7 +32,7 @@ describe("User case sign up, change password and delete account", function () {
         cy.mailslurp()
             // use inbox id and a timeout of 30 seconds, check unread mail only (set true)
             .then(mailslurp => mailslurp.waitForLatestEmail(
-                this.inboxId, 8000, true))
+                this.inboxId, 30000, true))
             // extract the confirmation code from the email body
             .then(email => new RegExp('accounts/confirm-email/.{43}').exec(email.body))
             // generate url with confirmation code to activate account and get token from cookies to use it below
@@ -49,12 +50,12 @@ describe("User case sign up, change password and delete account", function () {
     });
 
     it('04 - can reset password by email', function () {
-        cy.visit('');
+        cy.visit('/');
         command.forgotPassword(this.emailAddress)
         cy.mailslurp()
             // use inbox id and a timeout of 30 seconds, check unread mail only (set true)
             .then(mailslurp => mailslurp.waitForLatestEmail(
-                this.inboxId, 8000, true))
+                this.inboxId, 30000, true))
             // extract the confirmation code from the email body
             .then(email => new RegExp('accounts/password/reset/key/.{30}').exec(email.body))
             // generate url with confirmation code to activate account and get token from cookies to use it below
@@ -69,10 +70,11 @@ describe("User case sign up, change password and delete account", function () {
         cy.setCookie('Authorization', auth.token);
         cy.visit('/');
         command.changePassword()
-
+        //delete inbox after all jobs with it have finished
+        cy.mailslurp().then(mailslurp => mailslurp.deleteInbox(this.inboxId))
     });
 
-    it('05 - can delete user', function () {
+    it('06 - can delete user', function () {
         // set cookie to continue and delete the user
         cy.setCookie('Authorization', auth.token);
         cy.visit('/');
