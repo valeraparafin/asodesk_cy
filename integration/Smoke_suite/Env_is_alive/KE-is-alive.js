3222f841b488d5e5e063f407bce796800a52c113
@valeraparafin <iparafin@gmail.com>
2022-02-08 16:57:02 +0300

    [KE] fix related keyword parametr

--- integration/Smoke_suite/Env_is_alive/KE-is-alive.js
+++ integration/Smoke_suite/Env_is_alive/KE-is-alive.js
@@ -45,7 +45,7 @@ describe('Keyword Explorer should be alive and main requests should response 200
         cy.request({
             method: 'POST',
             followRedirect: true, log: true, //turn off
-            url: 'api/ru/com.bpmobile.iscanner.free/keyword-explorer/related?device_type=googleplay&keyword=%D0%BA%D0%B0%D0%BF%D0%B8%D1%82%D0%B0%D0%BB',
+            url: 'api/us/com.bpmobile.iscanner.free/keyword-explorer/related?device_type=googleplay&keyword=mcdonald',
             headers: {
                 'accept': 'application/json',
                 'Authorization': auth.token,
