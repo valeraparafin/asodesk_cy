e2abeaa172f9cab44db571075900316f8370ecb0
@valeraparafin <iparafin@gmail.com>
2022-03-05 12:35:10 +0300

    change cr test

--- integration/Smoke_suite/Env_is_alive/CR-is-alive.js
+++ integration/Smoke_suite/Env_is_alive/CR-is-alive.js
@@ -8,6 +8,7 @@ const auth = new Auth();
 
 const prevDaysDate = constant.prevDaysDate;
 const toDaysDate = constant.toDaysDate;
+const app = constant.applications.AS.snapchat
 
 describe('CR should be alive and main requests should response 200', function () {
     it('Obtain token', function () {
@@ -17,7 +18,7 @@ describe('CR should be alive and main requests should response 200', function ()
         cy.request({
             method: 'GET',
             followRedirect: true, log: true, //turn off
-            url: 'api/ru/1336519654/comparative-report-chart?time_since=' + prevDaysDate + '&time_till=' + toDaysDate,
+            url: 'api/ru/' + app + '/comparative-report-chart?time_since=' + prevDaysDate + '&time_till=' + toDaysDate,
             headers: {
                 'accept': 'application/json',
                 'Authorization': auth.token,
@@ -36,7 +37,7 @@ describe('CR should be alive and main requests should response 200', function ()
         cy.request({
             method: 'GET',
             followRedirect: true, log: true, //turn off
-            url: 'api/ru/1336519654/comparative-report?from_timestamp=' + prevDaysDate + '&to_timestamp=' + toDaysDate,
+            url: 'api/ru/' + app + '/comparative-report?from_timestamp=' + prevDaysDate + '&to_timestamp=' + toDaysDate,
             headers: {
                 'accept': 'application/json',
                 'Authorization': auth.token,
@@ -53,7 +54,7 @@ describe('CR should be alive and main requests should response 200', function ()
         cy.request({
             method: 'GET',
             followRedirect: true, log: true, //turn off
-            url: 'api/ru/1336519654/comparative-report-metas?from_timestamp=' + prevDaysDate + '&to_timestamp=' + toDaysDate,
+            url: 'api/ru/' + app + '/comparative-report-metas?from_timestamp=' + prevDaysDate + '&to_timestamp=' + toDaysDate,
             headers: {
                 'accept': 'application/json',
                 'Authorization': auth.token,
